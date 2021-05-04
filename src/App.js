719683e81da277fff2c84d61711f1f4dabffb078
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-05-04 16:47:05 +0300

    Successfully add task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
+import AddTask from './components/AddTask'
 
 function App() {
 
@@ -36,9 +37,16 @@ const toggleReminder = (id) => {
       {...task, reminder: !task.reminder}:
       task))
 }
+// add task
+const addTask = (task) => {
+  const id = Math.floor(Math.random()*1000) +1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask}/>
     { tasks.length >0 ? 
       (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />):
       ('No Taks to show')
